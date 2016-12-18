e7bc8206d3940ae21cba2105ea2bc8d75ce2ec1f
Lauren Bannon <x16399856@student.ncirl.ie>
2016-12-18 14:36:16 +0000

    Web Design Project

---
+++ bootstrap/js/script.js
@@ -0,0 +1,14 @@
+$('.carousel').carousel({
+  interval: 2000
+});
+ 
+$(document).ready(function(){
+   $("nav").sticky({topSpacing:0});
+});
+	
+function myFunction(){
+	alert("Divinity thanks you for your comment. We will be in touch.");
+}
+
+$("body").fadeIn(2000);
+	
